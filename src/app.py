27ffc1d8a0773c6451ce18896befa66e74695bab
Joshua Revilla <were1708@gmail.com>
2023-02-22 15:52:38 -0800

    added global variables

--- src/app.py
+++ src/app.py
@@ -1,1 +1,5 @@
 from flask import Flask, request, jsonify
+
+data = dict() # kvs!  you guys may wanna move this somewhere else!
+current_view = list() # will hold strings in format of "address1:port1" etc.
+
