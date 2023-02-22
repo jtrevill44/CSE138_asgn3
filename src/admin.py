dbc84c0448e59c98ae35ae9300aa80fb47fc2747
Joshua Revilla <were1708@gmail.com>
2023-02-22 12:24:32 -0800

    started admin tasks

--- src/admin.py
+++ src/admin.py
@@ -1,1 +1,15 @@
 from flask import Flask, request, jsonify
+
+
+@app.route('kvs/admin/view', methods = ['GET', 'PUT', 'DELETE'])
+def handle_views():
+
+    if request.method == 'GET':
+        return jsonify(view=current_view), 200
+
+    elif request.method == 'DELETE':
+        current_view.clear()
+        data.clear()
+        return 200;
+    elif request.method == 'PUT': # here comes all the complexity :)
+        
