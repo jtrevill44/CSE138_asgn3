04c419f5206df6170e422ddde516194ba2b8f95c
Mahesh-Vegiraju <vegiraju.mahesh@gmail.com>
2023-02-23 14:15:59 -0800

    adding broadcast

---
+++ src/broadcast.py
@@ -0,0 +1,23 @@
+async def async_request(ip, path, method, key, vector_clock):
+  url = f"http://{ip}{path}{method}/{key}"
+  state = {"causal-metadata" : vector_clock}
+  try:
+    if method == 'put':
+      return requests.put(url, json=state, timeout=(2/len(current_view)))
+    elif method == 'get':
+      return requests.get(url, json=state, timeout=(2/len(current_view)))
+    else:
+      return requests.delete(url, json=state, timeout=(2/len(current_view)))
+  except:
+    return "Timeout", 404
+
+# Input:
+#   method: the method on the end point in string form
+#   path: the path of the end point without the key (ie /internal/write/)
+#   key: the key in the kvs to modify/get
+#   vector_clock: the vector_clock of the node
+async def broadcast(method, path, key, vector_clock):
+  responses = []
+  for node in current_view:
+    await responses.append(async_request(node, path, method, key, vector_clock))
+  return responses
--- src/data_key_m.py
+++ src/data_key_m.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify, Blueprint
 import requests
 from globals import current_view, data, data_clocks, address
+from broadcast import broadcast
 
 blueprint = Blueprint('blueprint', __name__)
+
+def quorum_rep():
+  return 'hi'
 
 @blueprint.route('kvs/data/<key>', methods = ['PUT'])
 def handle_put(key):
   return 200
 
 
-@blueprint.route('kvs/data/rep/<key>', methods = ['PUT',])
+@blueprint.route('/internal/write/<key>', methods = ['PUT', 'DELETE'])
 def handle_rep_put(key):
   return 200
