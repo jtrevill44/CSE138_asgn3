fe19d33e7dbeb429a1cbee55ef0661a39d48562c
Joshua Revilla <were1708@yahoo.com>
2023-02-28 12:07:33 -0800

    internal broadcasts now check if the source was in the view

--- src/internal.py
+++ src/internal.py
@@ -4,17 +4,26 @@ import globals
 from broadcast import broadcast
 from vector_clocks import Vector_Clock
 
+
 @app.route('kvs/internal/broadcast', methods = ['PUT', 'DELETE'])
 def propogate_writes():
 
-     body = request.get_json()
-     other_clock = body.get('vector_clock')
-     key = body.get('key')
-     val = body.get('val')
+    body = request.get_json()
+    other_clock = body.get('vector_clock')
+    key = body.get('key')
+    val = body.get('val')
 
-     comparison = globals.data_clocks.compare(other_clock, key)
+    source_ip = request.remote_addr
+    source_port = "8080" # TODO might have to change this idk
+    source = f"{source_ip}:{source_port}" # we need to check if this address is in current_view
 
-     if comparison == 2 or comparison == -1: 
+    if source not in globals.current_view:
+        return 403 # node was not in the view!
+    
+
+    comparison = globals.data_clocks.compare(other_clock, key)
+
+    if comparison == 2 or comparison == -1: 
         # we can actually do the fucking operation!
         if request.method == 'PUT':
             if comparison == -1:
@@ -38,7 +47,7 @@ def propogate_writes():
                 return 200
             return 404
 
-     if comparison == 0 or comparison == 1:
+    if comparison == 0 or comparison == 1:
         # TODO
         # we have to do some searching here!
         # talk about this section in the meeting tomorrow
