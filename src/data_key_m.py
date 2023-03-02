36ad6ae78970929d6b5654ab6e9f8e8b2a7d8e46
Joshua Revilla <were1708@gmail.com>
2023-03-02 14:56:40 -0800

    added case for replica being ahead of client

--- src/data_key_m.py
+++ src/data_key_m.py
@@ -7,7 +7,7 @@ from vector_clocks import *
 blueprint = Blueprint('blueprint', __name__)
 EIGHT_MEGABYTES = 8388608
 
-@blueprint.route('kvs/data/<key>', methods = ['PUT'])
+@blueprint.route('kvs/data/<key>', methods = ['PUT', 'DELETE'])
 def handle_put(key):
 
   # get body and data
@@ -18,12 +18,7 @@ def handle_put(key):
   if len(val) > EIGHT_MEGABYTES:
     return jsonify(error="val too large"), 400
 
-
-  # set return code
   return_code = 200 if key in globals.local_data else 201
-  # update kvs
-  globals.local_data[key] = val
-
 
   # comparing vector clocks ##
   result = compare(globals.local_clocks, key, client_vc.get(key, []))
@@ -31,6 +26,8 @@ def handle_put(key):
     combine(globals.local_data, key, client_vc.get(key, []))
   elif result is -1: # if result is -1, ie the client's vector clock is greater than self's
     copy_key(globals.local_clocks, key, client_vc[key]) # set self's clock to that of the client
+  elif result is 1: # we were in the future, don't do anything and send our clock.
+    return jsonify({"causal-metadata" : globals.local_clocks}), return_code
 
   # update vc
   if key not in globals.local_clocks.keys(): # we need a new entry in the clocks
@@ -39,7 +36,7 @@ def handle_put(key):
   increment(globals.local_data, key, globals.node_id)
 
   # broadcast
-  responses = broadcast('PUT', '/internal/write/<key>', key, globals.local_clocks[key]) # change data_clocks[key] to the sending_vc
+  responses = broadcast(request.method, f'/internal/write/<{key}>', key, globals.local_clocks[key]) # change data_clocks[key] to the sending_vc
 
   return jsonify({"causal-metadata" : globals.local_clocks}), return_code
 
