767354186eec75775163e3bbee867c6eb4e62235
Joshua Revilla <were1708@gmail.com>
2023-03-01 18:24:21 -0800

    moved ID setting in admin where we actually get views

--- src/admin.py
+++ src/admin.py
@@ -3,6 +3,13 @@ import requests
 import globals
 
 
+def find_index() -> int:
+    for x in range(len(globals.current_view)):
+        if globals.current_view[x] == globals.address:
+            return x
+    return -1 
+
+
 admin = Blueprint("admin", __name__, url_prefix="/kvs/admin")
 
 @admin.route('/view', methods = ['GET', 'PUT', 'DELETE'])
@@ -30,6 +37,8 @@ def handle_views():
                 continue
         # Nodes that were to be deleted are now deleted!
         globals.current_view = new_view
+        globals.node_id = find_index() # get our new ID
+        
         for node in new_view: # send the new view and state to all nodes!
             if node == globals.address:
                 continue
@@ -53,4 +62,5 @@ def handle_update(): # function and end point for updating nodes with a view upd
     globals.current_view = body.get('view')
     globals.local_data = body.get('kvs')
     globals.local_clocks = body.get('vector_clock')
+    globals.node_id = find_index()
     return "", 200
--- src/app.py
+++ src/app.py
@@ -2,22 +2,13 @@ from flask import Flask, request, jsonify
 from admin import admin
 from internal import internal
 import os
-
 import globals
-
-def find_index() -> int:
-    for x in range(len(globals.current_view)):
-        if globals.current_view[x] == globals.address:
-            return x
-    return -1 
-
 
 
 app = Flask(__name__)
 
 try:
   globals.address = os.environ['ADDRESS']
-  globals.node_id = find_index()
 except:
   print("we didn't get an address!")
 
