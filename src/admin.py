a6260c25c907ea809dc77cf9115c47abb049e47a
Joshua Revilla <were1708@gmail.com>
2023-03-01 18:19:00 -0800

    added some trys and catches for sending messages

--- src/admin.py
+++ src/admin.py
@@ -24,15 +24,21 @@ def handle_views():
         deleted_nodes = [x for x in globals.current_view if x not in new_view] # nodes to delete
         for node in deleted_nodes:
             url = f"http://{node}/kvs/admin/view"
-            requests.delete(url)
+            try:
+                requests.delete(url, timeout=1)
+            except: # could be a partition or something, its fine!
+                continue
         # Nodes that were to be deleted are now deleted!
         globals.current_view = new_view
         for node in new_view: # send the new view and state to all nodes!
             if node == globals.address:
                 continue
             url = f"http://{node}/kvs/admin/update"
-            state = {"view":new_view, "data":globals.local_data}
-            requests.put(url, json=state, timeout=2)
+            state = {"view":new_view, "kvs":globals.local_data, "vector_clock":globals.local_clocks}
+            try:
+                requests.put(url, json=state, timeout=1)
+            except: 
+                continue
     
     else: # unsupported method!
         return "", 405
@@ -45,5 +51,6 @@ def handle_views():
 def handle_update(): # function and end point for updating nodes with a view update
     body = request.get_json()
     globals.current_view = body.get('view')
-    globals.local_data = body.get('data')
-    return 200
+    globals.local_data = body.get('kvs')
+    globals.local_clocks = body.get('vector_clock')
+    return "", 200
