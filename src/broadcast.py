f6a6d2a53253255f97dc6a2cee37c77b31d27edc
Joshua Revilla <were1708@yahoo.com>
2023-03-02 21:05:35 -0800

    fixed duplicate message json

--- src/broadcast.py
+++ src/broadcast.py
@@ -27,7 +27,7 @@ async def async_request(ip, path, method, key = None, vector_clock = dict(), val
 async def broadcast(method, path, key, vector_clock, val=None, node_id = globals.node_id, source = globals.address):
   tasks = []
   for node in globals.current_view:
-    task = asyncio.create_task(async_request(ip = node, path = path, method = method, key = key, vector_clock= vector_clock, val = val, node_id= node_id, key = key, source = source))
+    task = asyncio.create_task(async_request(ip = node, path = path, method = method, key = key, vector_clock= vector_clock, val = val, node_id= node_id, source = source))
     tasks.append(task)
   responses = await asyncio.gather(*tasks)
   return responses
