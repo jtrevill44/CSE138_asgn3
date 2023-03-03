d84091a13a03e41dfc5072dac14223d5e2d095b8
rmcclorey <ronan.mcclorey1@gmail.com>
2023-03-02 19:45:58 -0800

    Added update_known_clocks

--- src/globals.py
+++ src/globals.py
@@ -1,3 +1,5 @@
+from vector_clocks import compare, combine
+
 local_data = dict()
 current_view = list()
 local_clocks = dict()
@@ -6,4 +8,9 @@ last_write = dict()
 address = str()
 node_id = int(-1) # node starts off with no ID (uninit)
 
-
+def update_known_clocks(causal_metadata):
+    for key, clock in causal_metadata:
+        if key not in known_clocks:
+            known_clocks[key] = clock
+        else:
+            combine(known_clocks, key, clock)
