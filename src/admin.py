aaabfd81a2a0faa6fc35ef638dc734349b8ae5ec
Joshua Revilla <were1708@yahoo.com>
2023-03-10 14:45:56 -0800

    now extends vector clock when more nodes are added

--- src/admin.py
+++ src/admin.py
@@ -57,9 +57,9 @@ def handle_views():
 
         if len(globals.current_view) < len(new_view):
             for key in globals.local_clocks.keys():
-                globals.local_clocks[key].extend([0] * len(new_view) - len(globals.local_clocks))
+                globals.local_clocks[key].extend([0] * len(new_view) - len(globals.current_view))
             for key in globals.known_clocks.keys():
-                globals.known_clocks[key].extend([0] * len(new_view) - len(globals.known_clocks))
+                globals.known_clocks[key].extend([0] * len(new_view) - len(globals.current_view))
 
 
         globals.current_view = new_view
