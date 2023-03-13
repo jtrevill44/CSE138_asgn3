f2c08cda68cf73550ee5e577f41eaee528b6dbad
Joshua Revilla <were1708@yahoo.com>
2023-03-12 23:43:55 -0700

    added new globals for asgn4

--- src/globals.py
+++ src/globals.py
@@ -7,5 +7,9 @@ known_clocks = dict()
 last_write = dict()
 address = str()
 node_id = int(-1) # node starts off with no ID (uninit)
+shard_member = -1 # node starts off not in a shard
+shard_view = dict() # holds what nodes are in what shards! 
+# if you want the num of shards, take the len(globals.shard_view.keys())
+
 
 syncThread = threading.Thread()
