ce9ee353e7bfed6e95663ac66b6b9ae435ee4c8a
Joshua Revilla <were1708@yahoo.com>
2023-03-13 01:24:02 -0700

    added shard views need to add new admin get

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,30 @@
 from flask import Flask, request, jsonify, Blueprint
 import requests
 import globals
+import math
+
+
+def make_shard_view(view: list, num_shards: int) -> dict:
+    length = math.ceil(len(view)/num_shards)
+    return_dict = dict()
+    for i in range(num_shards):
+        return_dict[i] = list()
+    current_list = 0
+    iter_tracker = 0
+    for address in view:
+        iter_tracker += 1
+        return_dict[current_list].append(address)
+        if iter_tracker % length == 0:
+            current_list += 1
+    return return_dict
+    
+def find_shard(shard_view: dict) -> int:
+    for i in shard_view.keys():
+        for address in shard_view[i]:
+            if address == globals.address:
+                return i
+    return -1 # this should never happen!
+
 
 
 def find_index() -> int:
@@ -37,10 +61,11 @@ def handle_views():
     elif request.method == 'PUT': # here comes all the complexity :)
         body = request.get_json()
 
-        if 'view' not in body.keys() or len(body.keys()) > 1:
+        if 'view' not in body.keys() or len(body.keys()) > 1 or 'num_shards' not in body.keys():
             return jsonify({"error" : "bad request"}), 400
 
         new_view = body.get('view') # this is the new view!
+        num_shards = body.get('num_shards')
         deleted_nodes = [x for x in globals.current_view if x not in new_view] # nodes to delete
         for node in deleted_nodes:
             url = f"http://{node}/kvs/admin/view"
@@ -59,13 +84,15 @@ def handle_views():
 
         globals.current_view = new_view
         globals.node_id = globals.current_view.index(globals.address) # get our new ID
+        globals.shard_view = make_shard_view(globals.current_view, num_shards)
+        globals.shard_member = find_shard(globals.shard_view)
 
         
         for node in new_view: # send the new view and state to all nodes!
             if node == globals.address:
                 continue
             url = f"http://{node}/kvs/admin/update"
-            state = {"view":new_view, "kvs":globals.local_data, "vector_clock":globals.local_clocks, "known_clock" : globals.known_clocks}
+            state = {"view":new_view, "kvs":globals.local_data, "vector_clock":globals.local_clocks, "known_clock" : globals.known_clocks, "num_shards": num_shards}
             try:
                 requests.put(url, json=state, timeout=1)
             except: 
@@ -86,5 +113,9 @@ def handle_update(): # function and end point for updating nodes with a view upd
     globals.local_clocks = body.get('vector_clock')
     globals.known_clocks = body.get('known_clock')
     globals.node_id = find_index()
+    num_shards = body.get('num_shards')
+    globals.shard_view = make_shard_view(globals.current_view, num_shards)
+    globals.shard_member = find_shard(globals.shard_view)
+
     # globals.syncThread.start()
     return "", 200
