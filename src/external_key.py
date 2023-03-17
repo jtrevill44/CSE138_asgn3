6747451245fedf51f36c4a46f0054df015dbe7df
Joshua Revilla <were1708@gmail.com>
2023-03-16 17:16:34 -0700

    added update_known_clock

--- src/external_key.py
+++ src/external_key.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 import hashlib
 import asyncio
 from broadcast import broadcast_shard, broadcast_request
@@ -9,11 +9,23 @@ external_key = Blueprint('external_key', __name__, url_prefix='/kvs/data')
 
 @external_key.route('/<key>', methods = ['GET', 'PUT', 'DELETE'])
 def external_route(key):
+    body = request.get_json()
+    causal_metadata = body.get('causal-metadata', None)
+    update_known_clocks(causal_metadata)
+
     n = int(hashlib.sha256(key.encode()).hexdigest(),16) % len(globals.shard_view)
     responses = asyncio.run(broadcast_request(globals.shard_view.get(n, []), request, f"/internal/{key}"))
 
+    if request.method == "DELETE":
+        for i in responses: 
+            if i.status_code == 200:
+                return i
+        for i in responses:
+            if i != -1:
+                return i
+            
     for i in responses:
         if i != -1 and i.status_code != 500:
             return i
 
-    return "timeouterror msg" 
+    return jsonify({"causal-metadata" : globals.known_clocks, "error" : "timed out while waiting for depended updates"}), 500
